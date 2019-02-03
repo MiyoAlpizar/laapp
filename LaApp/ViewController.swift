c20c92357707189062788bdcc5594be28e351082
MiyoAlpizar <miyo.naval@gmail.com>
2019-02-02 18:57:30 -0600

    Initial Commit

---
+++ LaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LaApp
+//
+//  Created by Miyo Alpízar on 2/2/19.
+//  Copyright © 2019 Efrain Emigdio Navarrete Alpízar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
