92a3501ca9b16ea19feb28d4b2ef3608c7a18996
MiyoAlpizar <miyo.naval@gmail.com>
2019-02-02 19:42:42 -0600

    Firts Commit

---
+++ LaApp/Contacts/Controller/ContactsNC.swift
@@ -0,0 +1,30 @@
+//
+//  ContactsNC.swift
+//  LaApp
+//
+//  Created by Miyo Alpízar on 2/2/19.
+//  Copyright © 2019 Efrain Emigdio Navarrete Alpízar. All rights reserved.
+//
+
+import UIKit
+
+class ContactsNC: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LaApp/Extentions/ColorExtentios.swift
@@ -0,0 +1,9 @@
+//
+//  ColorExtentios.swift
+//  LaApp
+//
+//  Created by Miyo Alpízar on 2/2/19.
+//  Copyright © 2019 Efrain Emigdio Navarrete Alpízar. All rights reserved.
+//
+
+import Foundation
